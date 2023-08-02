fa045c53dd20c5a73b056a80152432380e056190
Ravi <hrithikr@UMICH.EDU>
2023-08-01 23:16:56 -0400

    Track validation with birds-eye POV demo video

--- draw_rectangle.m
+++ draw_rectangle.m
@@ -1,4 +1,4 @@
-function[]= draw_rectangle(Cx, Cy,L,H,theta,rgb, id)
+function[]= draw_rectangle(Cx, Cy,L,H,theta,rgb, id, egoToSensorMatrix, zLidar)
     R= ([cos(theta), -sin(theta); sin(theta), cos(theta)]);
     X=([-L/2, L/2, L/2, -L/2]);
     Y=([-H/2, -H/2, H/2, H/2]);
@@ -13,9 +13,19 @@ function[]= draw_rectangle(Cx, Cy,L,H,theta,rgb, id)
     y_lower_right=Cy+T(2,2);
     y_upper_right=Cy+T(2,3);
     y_upper_left=Cy+T(2,4);
-    x_coor=[x_lower_left x_lower_right x_upper_right x_upper_left];
-    y_coor=[y_lower_left y_lower_right y_upper_right y_upper_left];
-    patch('Vertices',[x_coor; y_coor]','Faces',[1 2 3 4],'Edgecolor',rgb,'Facecolor','none','Linewidth',1.2);
-    %text(Cx, Cy, id, 'Color', rgb, 'FontSize', 10);
+
+    egoX=[x_lower_left x_lower_right x_upper_right x_upper_left];
+    egoY=[y_lower_left y_lower_right y_upper_right y_upper_left];
+    transformedX = [];
+    transformedY = [];
+    for i=1:4
+        trans = egoToSensorMatrix*[egoX(i); egoY(i); -zLidar; 1];
+        transformedX = [transformedX trans(1)];
+        transformedY = [transformedY trans(2)];
+    end
+    
+    patch('Vertices',[transformedX; transformedY]','Faces',[1 2 3 4],'Edgecolor',rgb,'Facecolor','none','Linewidth',1.2);
+    textTrans = egoToSensorMatrix*[Cx+5; Cy+5; -zLidar; 1];
+    text(textTrans(1), textTrans(2), "Track " + id, 'Color', rgb, 'FontSize', 10);
     axis equal;
 end
--- readDBPlotTracks.m
+++ readDBPlotTracks.m
@@ -14,6 +14,29 @@ tic
 % Set random seed to generate reproducible results.
 S = rng(2018);
 
+sqlStr = sprintf('SELECT * FROM [Gttr4CPUv2].[dbo].[LidarExtrinsicCals] WHERE CalibrationId=(SELECT max(CalibrationId) FROM [Gttr4CPUv2].[dbo].[LidarExtrinsicCals]);');
+%curs = exec(conn,sqlStr);
+LidarCalibrationData = fetch(conn,sqlStr);
+LidarCalibrationDataStruct = table2struct(LidarCalibrationData);
+
+
+LidarMatricesStruct = struct();
+for row = 1:size(LidarCalibrationDataStruct, 1)
+    LidarMatricesStruct(row, 1).CamId = LidarCalibrationDataStruct(row).Lidar;
+    roll = deg2rad(LidarCalibrationDataStruct(row).RollLidar);
+    pitch = deg2rad(LidarCalibrationDataStruct(row).PitchLidar);
+    yaw = deg2rad(LidarCalibrationDataStruct(row).YawLidar);
+    X = LidarCalibrationDataStruct(row).XLidar;
+    Y = LidarCalibrationDataStruct(row).YLidar;
+    Z = LidarCalibrationDataStruct(row).ZLidar;
+    Rot_roll = [1 0 0; 0 cos(roll) -sin(roll); 0 sin(roll) cos(roll)];
+    Rot_pitch = [cos(pitch) 0 sin(pitch); 0 1 0; -sin(pitch) 0 cos(pitch)];
+    Rot_yaw = [cos(yaw) -sin(yaw) 0; sin(yaw) cos(yaw) 0; 0 0 1];
+    Rot_mat = Rot_yaw * Rot_pitch * Rot_roll;
+    LidarMatrix = [Rot_mat [X; Y; Z]; 0 0 0 1];
+    LidarMatricesStruct(row, 1).LidarMat = LidarMatrix;
+end
+
 
 % select the runids to process
 for i=260:260
@@ -119,16 +142,36 @@ for i=260:260
     rgb_tracks = rand(size(unique_tracks, 2), 3);
     all_tracks_X = [currTracks.X];
     all_tracks_Y = [currTracks.Y];
-    min_x = min([currTracks.X]) - 20;
-    max_x = max([max([currTracks.X]) + 20, 20]);
-    min_y = min([currTracks.Y]) - 20;
-    max_y = max([max([currTracks.Y]) + 20, 20]);
+    disp(currTracks(1));
+    disp(currTracks(2));
+    lidarID = 1;
+    egoToSensorMatrix = inv(LidarMatricesStruct(lidarID).LidarMat);
+    val = (min([currTracks.X]) - 20);
+    min_x = egoToSensorMatrix*[(min([currTracks.X]) - 20); 0; 0; 1]; % H - what should z be here? 0 or -zLidar?
+    min_x = min_x(1);
+    val = max([max([currTracks.X]) + 20, 20]);
+    max_x = egoToSensorMatrix*[(max([max([currTracks.X]) + 20, 20])); 0; 0; 1];
+    max_x = max_x(1);
+    min_y = egoToSensorMatrix*[0; (min([currTracks.Y]) - 20); 0; 1];
+    min_y = min_y(2);
+    max_y = egoToSensorMatrix*[0; (max([max([currTracks.Y]) + 20, 20])); 0; 1];
+    max_y = max_y(2);
     
     output_filename = 'images/track_result.gif';
     count = 1;
+
+    videoFile = 'lidar_tracks.mp4';
+    fps = 15;
+    video = VideoWriter(videoFile, 'MPEG-4');
+    video.FrameRate = fps;
+    open(video);
+
     for frameId = minFrame:maxFrame
         ind_d = find([Detections.Frame] == frameId);
         ind_t = find([currTracks.Frame] == frameId);
+        disp("frame " + frameId);
+%         disp("detections " + ind_d);
+        disp(size(ind_t, 1) + " tracks " + ind_t);
 
         if isempty(ind_d) || isempty(ind_t)
             continue;
@@ -139,37 +182,51 @@ for i=260:260
 %         disp(size(cell2mat({Detections(ind_d).TimeGps})));
 
         TimeGpsDetectionArr = cell2mat({Detections(ind_d).TimeGps});
-        disp(size(unique(TimeGpsDetectionArr))); %always 1x1? if not, what would we subtract from TimeGpsArr in the next line?
+        disp(size(unique(TimeGpsDetectionArr))); % always 1x1? if not, what would we subtract from TimeGpsArr in the next line?
         [~, ind_pc] = min(abs(TimeGpsArr - TimeGpsDetectionArr(1)));
         las = lasFileReader(Lidar_folder + sprintf('%06d', Count(ind_pc)) + ".laz");
         pc = readPointCloud(las);
+%         player = pcplayer([min([min_x max_x]) max([min_x max_x])], [min([min_y max_y]) max([min_y max_y])], [-30 60]);
+%         view(player,pc); 
         pcshow(pc);
+        azimuthAngle = 120;   % Horizontal rotation around z-axis (degrees)
+        elevationAngle = 120; % Vertical rotation around x-axis (degrees)
+        
+        % Rotate the view using camorbit
+        camorbit(azimuthAngle, elevationAngle, 'camera');
 %        plot(0, 0, 'bo', 'MarkerSize', 20);
         
-        for j = 1:size(ind_d, 1)
-            id = sprintf('O%d', Detections(ind_d(j)).ObjCnt);
-            rgb = [rand, rand, rand];
-            draw_rectangle(Detections(ind_d(j)).XTV,...
-                           Detections(ind_d(j)).YTV,...
-                           Detections(ind_d(j)).Sx,...
-                           Detections(ind_d(j)).Sy,...
-                           Detections(ind_d(j)).RotV,...
-                           rgb,... 
-                           id);
-            axis([min_x max_x min_y max_y])
-        end
-        for j = 1:size(ind_t, 1)
+%         for j = 1:size(ind_d, 2)
+%             id = sprintf('O%d', Detections(ind_d(j)).ObjCnt);
+%             rgb = [rand, rand, rand];
+%             draw_rectangle(Detections(ind_d(j)).XTV,...
+%                            Detections(ind_d(j)).YTV,...
+%                            Detections(ind_d(j)).Sx,...
+%                            Detections(ind_d(j)).Sy,...
+%                            Detections(ind_d(j)).RotV,...
+%                            rgb,... 
+%                            id, ...
+%                            egoToSensorMatrix, ...
+%                            egoToSensorMatrix(3, 4));
+%             axis([min([min_x max_x]) max([min_x max_x]) min([min_y max_y]) max([min_y max_y]) -30 60])
+%             %axis([0 500 0 500]);
+%         end
+        for j = 1:size(ind_t, 2)
             id = sprintf('%d', currTracks(ind_t(j)).TrackId);
             unique_ind = find(currTracks(ind_t(j)).TrackId == unique_tracks);
             rgb = rgb_tracks(unique_ind, :);
+            disp(currTracks(ind_t(j)));
             draw_rectangle(currTracks(ind_t(j)).X,...
                            currTracks(ind_t(j)).Y,...
                            currTracks(ind_t(j)).Sx,...
                            currTracks(ind_t(j)).Sy,...
                            currTracks(ind_t(j)).Rot,...
                            rgb,... 
-                           id);
-            axis([min_x max_x min_y max_y])
+                           id, ...
+                           egoToSensorMatrix, ...
+                           egoToSensorMatrix(3, 4));
+            axis([min([min_x max_x]) max([min_x max_x]) min([min_y max_y]) max([min_y max_y]) -30 60])
+            %axis([0 100 0 100 -20 100]);
         end
         f = getframe(fig); 
         im = frame2im(f); 
@@ -180,7 +237,10 @@ for i=260:260
             imwrite(imind,cm,output_filename,'gif','WriteMode','append'); 
         end
         pause(0.1);
+        frame = getframe(gcf);    
+        writeVideo(video, frame);
         clf(fig);
         count = count + 1;
     end
+    close(video);
 end
